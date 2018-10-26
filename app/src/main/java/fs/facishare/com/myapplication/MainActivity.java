8d73ff06bbffda87b360aad88a57378fbbdd6517
yuanh <yuanh@fxiaoke.com>
2018-10-26 18:43:21 +0800

    FristCommit

---
+++ app/src/main/java/fs/facishare/com/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2018 Facishare Technology Co., Ltd. All Rights Reserved.
+ */
+package fs.facishare.com.myapplication;
+
+import com.bumptech.glide.Glide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import retrofit2.Retrofit;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
